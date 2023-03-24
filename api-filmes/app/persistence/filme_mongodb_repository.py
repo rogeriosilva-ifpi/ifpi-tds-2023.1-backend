e8942a2326835fc14454880d277c986019537617
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2023-03-24 20:32:34 -0300

    Add docker

--- api-filmes/app/persistence/filme_mongodb_repository.py
+++ api-filmes/app/persistence/filme_mongodb_repository.py
@@ -19,13 +19,14 @@ class FilmeMongoDBRepository():
     def __init__(self):
         # Connect to MongoDB
         # uri = 'mongodb://localhost:27017'
-        uri = 'mongodb+srv://rogeriosilva:gB8KcjoiVMiL73mI@cluster0.v3apzvv.mongodb.net/?retryWrites=true&w=majority'
+        uri = 'mongodb://mongodb_bitnami:27017'
+        # uri = 'mongodb+srv://rogeriosilva:gB8KcjoiVMiL73mI@cluster0.v3apzvv.mongodb.net/?retryWrites=true&w=majority'
         client = MongoClient(uri)
         db = client['filmesapp']
         self.filmes = db['filmes']
         try:
             # print('Info MongoDB Server: ', client.server_info())
-            print('MongoDB 💖')
+            print('MongoDB 💖✅')
         except Exception:
             print('Deu erro!')
 
