1055b3a718935fb8a88239c2ccaa653c6dd0b87c
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2023-03-24 22:55:17 -0300

    Add front url on CORS list

--- api-filmes/main.py
+++ api-filmes/main.py
@@ -5,7 +5,9 @@ from app.presentation.controllers import filme_controller, usuario_controller
 
 app = FastAPI()
 
-origins = ['http://localhost:5500', 'http://127.0.0.1:5500']
+origins = ['http://localhost:5500',
+           'http://127.0.0.1:5500',
+           'https://ifpi-tds-2023-1-backend.pages.dev']
 
 app.add_middleware(CORSMiddleware,
                    allow_origins=origins,
